ac1b3bec8fc129d2faf642aa96eacf34811924da
Peter Swan <pdswan@gmail.com>
2015-03-24 22:50:05 -0400

    add Unsound::Control.maybe, closes #4

--- lib/unsound/control.rb
+++ lib/unsound/control.rb
@@ -16,5 +16,19 @@ module Unsound
     rescue
       Data::Left.new($ERROR_INFO)
     end
+
+    # Execute the block. If the block
+    # results in Nil, return {Data::Nothing},
+    # otherwise wrap the result in a {Data::Just}
+    #
+    # @param block [Block] the block to execute
+    # @return [Data::Nothing, Data::Just] an instance of {Data::Maybe}
+    def maybe(&block)
+      if (result = block.call).nil?
+        Data::Nothing.new
+      else
+        Data::Just.new(result)
+      end
+    end
   end
 end
